e1c15dcc1ac952841f085bb0554c87277326de9d
Nagyandb <nagyandb@gmail.com>
2022-09-03 11:28:09 +0200

    updates

--- src/main/java/app/controllers/SpaceShipController.java
+++ src/main/java/app/controllers/SpaceShipController.java
@@ -2,12 +2,24 @@ package app.controllers;
 
 import app.models.SpaceShip;
 import app.models.SpaceShipClass;
+import app.repositories.SpaceShipRepo;
+import app.services.SpaceShipService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class SpaceShipController {
+
+    private SpaceShipRepo spaceShipRepo;
+
+    @Autowired
+    public SpaceShipController(SpaceShipRepo spaceShipRepo) {
+        this.spaceShipRepo = spaceShipRepo;
+    }
 
     @GetMapping(value = {"/spaceship"})
     public String saveNewSpaceShip(Model model) {
@@ -15,6 +27,15 @@ public class SpaceShipController {
         model.addAttribute("shipClasses", SpaceShipClass.values());
 
         return "spaceship_new";
+    }
+
+    @GetMapping (value = {"/allspaceships"})
+    public String saveAllShips(Model model){
+
+        List<SpaceShip> allSpaceShips = (List<SpaceShip>) spaceShipRepo.findAll();
+        model.addAttribute("allSpaceShips", allSpaceShips);
+
+        return "allspaceships";
     }
 
 }
--- src/main/java/app/repositories/SpaceShipRepo.java
+++ src/main/java/app/repositories/SpaceShipRepo.java
@@ -2,6 +2,8 @@ package app.repositories;
 
 import app.models.SpaceShip;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SpaceShipRepo extends CrudRepository<SpaceShip, Long> {
 }
